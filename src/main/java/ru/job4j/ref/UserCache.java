5c13b4cff4e1d2fc73a37ca4ece844dceffd5c82
Lyubov <n.lubovj@gmail.com>
2024-05-29 23:55:14 +0300

    Add findAll() method to UserCache class

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -1,6 +1,8 @@
 package ru.job4j.ref;
 
 import javax.annotation.concurrent.ThreadSafe;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,7 +16,16 @@ public class UserCache {
     }
 
     public User findById(int id) {
+        if (!users.containsKey(id)) {
+            throw new NoSuchElementException();
+        }
         return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return users.values().stream()
+                .map(u -> User.of(u.getName()))
+                .toList();
     }
 
 }
