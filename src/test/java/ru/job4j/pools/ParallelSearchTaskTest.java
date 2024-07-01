929c131d911021a8ec95d026fabc60bec22d9da1
Lyubov <n.lubovj@gmail.com>
2024-07-01 22:02:37 +0300

    Correct ParallelSearchTaskTest class, add new tests to it

--- src/test/java/ru/job4j/pools/ParallelSearchTaskTest.java
+++ src/test/java/ru/job4j/pools/ParallelSearchTaskTest.java
@@ -7,57 +7,90 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ParallelSearchTaskTest {
 
     @Test
-    public void findIndexByKeyWhenInteger() {
+    public void linearSearchWhenIntegerArray() {
         Integer[] array = new Integer[]{1, 2, 3, 4, 5, 6};
 
-        long result = ParallelSearchTask.findIndexByKey(array, 3, 0, array.length);
+        int result = ParallelSearchTask.findIndexByKey(array, 3, 0, array.length);
 
         assertEquals(2, result);
     }
 
     @Test
-    public void findIndexByKeyWhenIntegerAndTwoKeysThenFindFirst() {
+    public void linearSearchWhenIntegerArrayAndFoundTwoKeysThenReturnFirst() {
         Integer[] array = new Integer[]{1, 3, 3, 4, 5, 6};
 
-        long result = ParallelSearchTask.findIndexByKey(array, 3, 0, array.length);
+        int result = ParallelSearchTask.findIndexByKey(array, 3, 0, array.length);
 
         assertEquals(1, result);
     }
 
     @Test
-    public void findIndexByKeyFromMidWhenInteger() {
+    public void linearSearchFromMidWhenIntegerArray() {
         Integer[] array = new Integer[]{1, 3, 3, 4, 5, 6};
 
-        long result = ParallelSearchTask.findIndexByKey(array, 3, 2, array.length);
+        int result = ParallelSearchTask.findIndexByKey(array, 3, 2, array.length);
 
         assertEquals(2, result);
     }
 
     @Test
-    public void findIndexByKeyWhenString() {
+    public void linearSearchWhenStringArray() {
         String[] array = new String[]{"a", "key", "c", "d", "e"};
 
-        long result = ParallelSearchTask.findIndexByKey(array, "key", 0, array.length);
+        int result = ParallelSearchTask.findIndexByKey(array, "key", 0, array.length);
 
         assertEquals(1, result);
     }
 
     @Test
-    public void findIndexByKeyWhenStringAndTwoKeysThenFindFirst() {
+    public void linearSearchWhenStringArrayAndFoundTwoKeysThenReturnFirst() {
         String[] array = new String[]{"a", "b", "key", "c", "key"};
 
-        long result = ParallelSearchTask.findIndexByKey(array, "key", 0, array.length);
+        int result = ParallelSearchTask.findIndexByKey(array, "key", 0, array.length);
 
         assertEquals(2, result);
     }
 
     @Test
-    public void findIndexByKeyFromMidWhenString() {
+    public void linearSearchFromMidWhenWhenStringArray() {
         String[] array = new String[]{"a", "key", "c", "key", "d"};
 
-        long result = ParallelSearchTask.findIndexByKey(array, "key", 2, array.length);
+        int result = ParallelSearchTask.findIndexByKey(array, "key", 2, array.length);
 
         assertEquals(3, result);
+    }
+
+    @Test
+    public void recursiveSearchWhenIntegerArray() {
+        Integer[] array = new Integer[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        int result = ParallelSearchTask.findIndexByKey(array, 13, 0, array.length);
+
+        assertEquals(13, result);
+    }
+
+    @Test
+    public void recursiveSearchWhenStringArray() {
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = String.valueOf(i);
+        }
+
+        int result = ParallelSearchTask.findIndexByKey(array, "13", 0, array.length);
+
+        assertEquals(13, result);
+    }
+
+    @Test
+    public void findIndexByKeyWhenNoSuchKey() {
+        Integer[] array = {1, 2, 3, 4, 5};
+
+        int result = ParallelSearchTask.findIndexByKey(array, 6, 0, array.length);
+
+        assertEquals(-1, result);
     }
 
 }
