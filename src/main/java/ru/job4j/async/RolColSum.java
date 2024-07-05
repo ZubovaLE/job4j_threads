f739d7b85b11b206bc7f0acf4a6afaf49b4b5525
Lyubov <n.lubovj@gmail.com>
2024-07-05 22:36:57 +0300

    Add asyncSum method to RolColSum class; Improve sum method

--- src/main/java/ru/job4j/async/RolColSum.java
+++ src/main/java/ru/job4j/async/RolColSum.java
@@ -2,38 +2,58 @@ package ru.job4j.async;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.concurrent.CompletableFuture;
 
 public class RolColSum {
 
     public static Sums[] sum(int[][] matrix) {
         int n = matrix.length;
-        int[] rowSums = new int[n];
-        int[] colSums = new int[n];
+        Sums[] results = new Sums[n];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                rowSums[i] += matrix[i][j];
-                colSums[j] += matrix[i][j];
-            }
+            results[i] = calculateSums(matrix, i);
         }
 
-        Sums[] result = new Sums[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = new Sums(rowSums[i], colSums[i]);
-        }
-
-        return result;
+        return results;
     }
 
+    public static Sums[] asyncSum(int[][] matrix) {
+        int n = matrix.length;
+        Sums[] results = new Sums[n];
+        CompletableFuture<Void>[] futures = new CompletableFuture[n];
 
-//    public static Sums[] asyncSum(int[][] matrix) {
-//
-//    }
+        for (int i = 0; i < n; i++) {
+            results[i] = new Sums();
+            final int index = i;
+            futures[i] = CompletableFuture.runAsync(() -> {
+                results[index] = calculateSums(matrix, index);
+            });
+        }
+
+        CompletableFuture.allOf(futures).join();
+        return results;
+    }
+
+    private static Sums calculateSums(int[][] matrix, int index) {
+        int rowSum = 0;
+        int colSum = 0;
+        for (int j = 0; j < matrix.length; j++) {
+            rowSum += matrix[index][j];
+            colSum += matrix[j][index];
+        }
+        Sums sums = new Sums();
+        sums.setRowSum(rowSum);
+        sums.setColSum(colSum);
+        return sums;
+    }
 
     @Getter
     @Setter
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class Sums {
 
         private int rowSum;
